6f95afea9c768a14d7f60a84d310cedcd5dd5dff
lucasfrancoborges123 <127310319+lucasfrancoborges123@users.noreply.github.com>
2024-11-22 15:30:57 -0300

    Update cachorro.py

--- cachorro.py
+++ cachorro.py
@@ -1,1 +1,13 @@
-
+from animal import Animal
+class Cachorro(Animal):
+    def __init__(self, nome, idade, porte):
+        super().__init__(nome, idade)
+        self.__porte=porte
+        
+    def setporte(self, porte):
+        self.__porte=porte
+    def getporte(self):
+        return self.__porte
+    def mostar(self):
+        return (f"O animal se chama: {self.getnome()}, idade {self.getidade()}, Porte: {self.getporte()}")
+    
