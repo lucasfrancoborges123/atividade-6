d8b511614c0bff4bfd53714aca6cd006417a0424
lucasfrancoborges123 <127310319+lucasfrancoborges123@users.noreply.github.com>
2024-11-22 15:30:37 -0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,1 +1,19 @@
-
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    def __init__(self, nome, idade):
+        self.__nome=nome
+        self.__idade=idade
+        
+    def getnome(self):
+        return self.__nome
+    def setNome(self, nome):
+        self.__nome=nome
+    
+    def getidade(self):
+        return self.__idade
+    def setIdade(self, idade):
+        self.__idade=idade
+        
+    @abstractmethod
+    def mostar(self):
+        pass 
