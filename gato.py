e18d86c02282962ddc6ba8e2ada6030da9dcf9d7
lucasfrancoborges123 <127310319+lucasfrancoborges123@users.noreply.github.com>
2024-11-22 15:32:02 -0300

    Update gato.py

--- gato.py
+++ gato.py
@@ -1,1 +1,12 @@
-
+from animal import Animal
+class Gato (Animal):
+    def __init__(self, nome, idade, raca):
+        super().__init__(nome, idade)
+        self.__raca=raca
+        
+    def setraca(self, raca):
+        self.__raca=raca
+    def getraca(self):
+        return self.__raca
+    def mostar(self):
+        return (f"O animal se chama: {self.getnome()}, idade {self.getidade()}, Raça: {self.getraca()},")
